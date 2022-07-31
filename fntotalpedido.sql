e5f8da46cd984bb31b7594bdbad0979fb8d44607
Wanda Nicole Bravo <86751741+wnicolebravo@users.noreply.github.com>
2022-07-31 13:45:02 -0300

    Add files via upload

--- fntotalpedido.sql
+++ fntotalpedido.sql
@@ -1,7 +1,11 @@
+-- Creamos la accion Total de pedidos para saber la cantidad de ellos en la tabla Pedidos
 CREATE DEFINER=`root`@`localhost` FUNCTION `TotalPedidos`( ped INT) RETURNS int
+
+-- determinamos el tipo de funcion
     DETERMINISTIC
+    -- Inicio de acciones
 BEGIN
-	DECLARE Ped INT;
+	DECLARE Ped INT; 
     SELECT COUNT(*) INTO Ped FROM Pedido;
     RETURN Ped;
 END
